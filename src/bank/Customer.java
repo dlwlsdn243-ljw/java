8d538de801069fd8824be134f134738f2da5056c
SNGOceans <hyuk2068@gmail.com>
2026-04-14 11:53:02 +0900

    test

--- src/bank/Customer.java
+++ src/bank/Customer.java
@@ -13,13 +13,6 @@ public class Customer {
 //	- 입금, 출금, 송금시 본인계좌 파일로 기록
 //	- 파일이름은 본인이름_은행이름
 	
-//	ex)
-//	입금 : 10000원, 현재 잔액 = 20000원
-//	출금 : 5000원, 현재 잔액 = 15000원
-//	송금 ← 이순신 : 20000원, 현재 잔액 = 25000원
-//	송금 ← 홍길동 : 5000원,  현재잔액: 15000원
-//	입금 : 10000원,  현재잔액: 25000원
-//	입금 : 20000원, 현재 잔액 = 30000원
 	
 	private String customerName; // 고객 이름
 	private String customerBank; // 고객 은행
@@ -36,11 +29,7 @@ public class Customer {
 		this.money = money;
 	}
 	
-	public void addCustomer() {
-		list.add(new Customer("홍길동", "우리은행", "111-11111-11111"));
-		list.add(new Customer("철수", "신한은행", "2222-22222-222221"));
-		list.add(new Customer("유리", "우리은행", "333-33311-1133331"));
-	}
+
 	
 	public String getCustomerName() {
 		return customerName;
